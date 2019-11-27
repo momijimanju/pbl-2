6d508b3002dedc7de8797accb68091a092cd17c3
wgdp444 <tatsuyori.shiraga@triple-four.com>
2019-11-27 10:42:42 +0900

    .gitignore

--- pbl/reception/views.py
+++ pbl/reception/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views import View
-from django.views.generic import View, TemplateView, CreateView
+from django.views.generic import View, TemplateView, CreateView, DetailView
 from .forms import FirstEntryFrom
 from .models import JoinUser
 
@@ -14,14 +14,24 @@ class FirstEntryFormView(CreateView):
   form_class = FirstEntryFrom
   
 first_entry_form = FirstEntryFormView.as_view()
+# class ProblemGroupDetailView(DetailView):
+#     template_name = "triplefour/problem/group/detail.html"
+#     model = JoinUser
+
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         # context['preblem_group'] = QuestionGroup.objects.filter(id=self.kwargs['pk'])
+        
+#         context['problem_group'] = JoinUser.objects.get(id=self.kwargs['pk'])
+#         return context
 # # 参加表入力(初回)確認画面
-# class firstFormDetailView():
+# class firstFormDetailView(DetailView):
 #   pass
 # # 参加表入力(2回目)画面
-# class secondFormView():
+# class secondFormView(CreateView):
 #   pass
 # # 参加表入力(2回目)確認画面
-# class secondFormDetailView():
+# class secondFormDetailView(DetailView):
 #   pass
 
 
