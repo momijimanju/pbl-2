57477c3f0086f8d2d6f50e11fe5949946b6c51c6
wgdp444 <tatsuyori.shiraga@triple-four.com>
2019-11-27 11:37:45 +0900

    url.py修正

--- pbl/reception/urls.py
+++ pbl/reception/urls.py
@@ -6,8 +6,8 @@ app_name = 'reception'
 
 urlpatterns = [
   path('firstform/', views.first_entry_form, name='first_entry_form'),
-  path('firstform/', views.first_entry_form, name='first_entry_form_detail'),
-  path('firstform/', views.first_entry_form, name='second_entry_form'),
-  path('firstform/', views.first_entry_form, name='second_entry_form_detail'),
-  path('firstform/', views.first_entry_form, name='join_confirm'),
+  path('firstform/detail', views.first_entry_form_detail, name='first_entry_form_detail'),
+  path('secondform/', views.second_entry_form, name='second_entry_form'),
+  path('secondform/detail', views.second_entry_form_detail, name='second_entry_form_detail'),
+  path('joinconfirm/', views.join_confirm, name='join_confirm'),
 ]
