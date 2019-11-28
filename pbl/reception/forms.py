3a197363bbf1e7426c46712a1d836bd42c855f40
wgdp444 <tatsuyori.shiraga@triple-four.com>
2019-11-28 09:36:53 +0900

    form.py更新

--- pbl/reception/forms.py
+++ pbl/reception/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from .models import JoinUser
+import datetime
 
 # 初回参加者入力画面のフォーム
 class FirstEntryFrom(forms.ModelForm):
@@ -12,7 +13,9 @@ class FirstEntryFrom(forms.ModelForm):
     model = JoinUser
     fields = ('first_name', 'last_name','read_first_name', 'read_last_name',
                'birthday', 'gender', 'phone_number', 'postal_code')
+    
     widgets = {
-            'birthday': forms.SelectDateWidget
+            'birthday': forms.SelectDateWidget(years=[x for x in range(1950, datetime.datetime.now().year + 1)]),
+            'gender': forms.RadioSelect
     }
 
