fa7d4eaa28e84d3cf76c4872f3724d2ae0a5ef62
wgdp444 <tatsuyori.shiraga@triple-four.com>
2019-12-01 12:51:13 +0900

    formの作成とマイグレーション

--- pbl/reception/forms.py
+++ pbl/reception/forms.py
@@ -6,11 +6,11 @@ import datetime
 class FirstEntryFrom(forms.ModelForm):
   def __init__(self, *args, **kwargs):
     super().__init__(*args, **kwargs)
-    # 郵便番号を入力すると住所を地頭的に入力するための属性指定
+    # 郵便番号を入力すると住所を自動的に入力するための属性指定
     self.fields['postal_code'].widget = forms.TextInput(attrs={
       'onKeyUp': "AjaxZip3.zip2addr(this,'','street_address','street_address');",
-      'class': 'form-control',
     })
+    # self.fields['street_address'].widget.attrs['readonly'] = True
     for field in self.fields.values():
       field.widget.attrs['class'] = 'form-control'
 
@@ -18,11 +18,13 @@ class FirstEntryFrom(forms.ModelForm):
     model = JoinUser
     fields = ('first_name', 'last_name','read_first_name', 'read_last_name',
                'birthday', 'gender', 'phone_number', 'postal_code', 'street_address', 
-               'profession',
+               'profession', 'school_year', 'graduation_year', 'graduate', 'certification',
+               'high_school_name',
              )
     
     widgets = {
             'birthday': forms.SelectDateWidget(years=[x for x in range(1950, datetime.datetime.now().year + 1)]),
             'gender': forms.RadioSelect,
+            'profession': forms.RadioSelect,
     }
 
