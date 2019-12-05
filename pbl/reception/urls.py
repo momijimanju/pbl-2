75d29b59cf42502443ee920ae1bf5a437cd353f3
ryota1110 <57474990+ryota1110@users.noreply.github.com>
2019-12-05 13:47:02 +0900

    とりあえず共有

--- pbl/reception/urls.py
+++ pbl/reception/urls.py
@@ -9,8 +9,8 @@ urlpatterns = [
   path('firstform/test_success/', views.test_success, name='test_success'),
   # path('firstform/test_form/', views.first_entry_form, name='first_entry_form_view'),
   # 藤野
-  # path('secondform/', views.second_entry_form, name='second_entry_form'),
-  # path('secondform/detail', views.second_entry_form_detail, name='second_entry_form_detail'),
+  path('secondform/', views.second_entry_form, name='second_entry_form'),
+  # path('secondform/detail/<int:pk>', views.second_entry_form_detail, name='second_entry_form_detail'),
   # シラガ
   # path('joinconfirm/', views.join_confirm, name='join_confirm'),
 ]
